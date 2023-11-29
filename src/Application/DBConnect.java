191610938cc5b6143045c58f22b48cf188db741c
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-11-29 05:16:19 +0000

    Update to v2

--- src/Application/DBConnect.java
+++ src/Application/DBConnect.java
@@ -1,13 +1,21 @@
 package Application;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
 public class DBConnect {
     public static Connection connect() {
+        Properties prop = new Properties();
+        String fileName = "app.config";
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gym_management_system", "admin", "admin");
+            prop.load(new FileInputStream(fileName));
+            String database_name = prop.getProperty("app.database_name");
+            String user = prop.getProperty("app.user");
+            String password = prop.getProperty("app.password");
+            System.out.println("Database Name: " + database_name + "\n" + "User: " + user + "\n" + "Password: " + password);
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database_name, user, password);
             return conn;
         } catch (Exception e) {
             e.printStackTrace();
@@ -16,16 +24,3 @@ public class DBConnect {
         }
     }
 }
-
-//    Properties prop = new Properties();
-//    String fileName = "app.config";
-//            try {
-//                    prop.load(new FileInputStream(fileName));
-//                    } catch (Exception e) {
-//                    e.printStackTrace();
-//                    }
-//                    System.out.println(prop.getProperty("app.user"));
-//                    System.out.println(prop.getProperty("app.password"));
-//                    DBConnect con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gym_management_system","admin","admin");
-//                    System.out.println(con);
-//                    System.out.println("All ok");
